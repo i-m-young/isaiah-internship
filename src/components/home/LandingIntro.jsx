f31b4684176926749b78df09e93cc4877f4dbde3
i-m-young <youngisaiahm@gmail.com>
2026-09-08 09:47:54 -0700

    Installed animate on scroll library, added animations to landing page including intro, hot collections, new items, top sellers, and browse by category

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -7,39 +7,60 @@ const LandingIntro = () => {
         <div className="row">
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_wallet"></i>
-              <div className="text">
-                <h4 className="">Set up your wallet</h4>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem.
-                </p>
+              <div
+                data-aos="fade-up"
+                data-aos-delay="250"
+                data-aos-duration="500"
+                data-aos-easing="ease-in-out"
+              >
+                <i className="bg-color-2 i-boxed icon_wallet"></i>
+                <div className="text">
+                  <h4 className="">Set up your wallet</h4>
+                  <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium, totam rem.
+                  </p>
+                </div>
               </div>
               <i className="wm icon_wallet"></i>
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
-              <div className="text">
-                <h4 className="">Add your NFT's</h4>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem.
-                </p>
+              <div
+                data-aos="fade-up"
+                data-aos-delay="250"
+                data-aos-duration="500"
+                data-aos-easing="ease-in-out"
+              >
+                <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
+                <div className="text">
+                  <h4 className="">Add your NFT's</h4>
+                  <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium, totam rem.
+                  </p>
+                </div>
               </div>
               <i className="wm icon_cloud-upload_alt"></i>
             </div>
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_tags_alt"></i>
-              <div className="text">
-                <h4 className="">Sell your NFT's</h4>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem.
-                </p>
+              <div
+                data-aos="fade-up"
+                data-aos-delay="250"
+                data-aos-duration="500"
+                data-aos-easing="ease-in-out"
+              >
+                <i className="bg-color-2 i-boxed icon_tags_alt"></i>
+                <div className="text">
+                  <h4 className="">Sell your NFT's</h4>
+                  <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium, totam rem.
+                  </p>
+                </div>
               </div>
               <i className="wm icon_tags_alt"></i>
             </div>
--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -28,7 +28,13 @@ const NewItems = () => {
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
-        <div className="row">
+        <div
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-out"
+          data-aos-delay="250"
+          data-aos-duration="500"
+          className="row"
+        >
           <div className="col-lg-12">
             <div className="text-center">
               <h2>New Items</h2>
